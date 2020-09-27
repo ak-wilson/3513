52f81fd13b64693fa7464d8a5de458014242f752
akwilson <awilso92@alumni.jh.edu>
2020-09-27 00:48:05 +0000

    merge

---
+++ test/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, jsonify
+
+app = Flask(__name__)
+
+def do_something(text1,text2):
+    text1 = text1.upper()
+    text2 = text2.upper()
+    combine = text1 + text2
+    return combine
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    text1 = request.form['text1']
+    word = request.args.get('text1')
+    text2 = request.form['text2']
+    combine = do_something(text1,text2)
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
